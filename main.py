93db16a84f880008be32f6bf641e766be1cf0a23
PedroLedoBustos <pedroldesign@gmail.com>
2024-06-17 13:24:55 +0200

    Modificado main

--- main.py
+++ main.py
@@ -3,3 +3,31 @@ def menu():
     print("###############################")
     print("######## MENU PRINCIPAL #######")
     print("###############################")
+    print("1 Alta Equipo")
+    print("2 Alta Equipo")
+    print("3 Ver jugadores mas altos")
+    print("4 Lista entrenadores ordenada por antiguedad")
+    opcion= input("Elige una opción: ")
+
+    if opcion == "1":
+        liga.altaEquipo()
+    elif opcion == "2":
+        liga.bajaEquipo()
+    elif opcion == "3":
+        liga.verJugadoresAltos()
+    elif opcion == "4":
+        liga.verEntrenadores()
+    elif opcion == "5":
+        salir= True
+    else:
+        print("Opción escogida no válida")
+    
+    return salir
+
+def aplicacion():
+    salir= False
+    while salir== False:
+        salir= menu()
+
+
+aplicacion()         
